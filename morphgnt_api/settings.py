4612198bf6720fbca7343fee8da612d7bf1ac38e
James Tauber <jtauber@jtauber.com>
2015-07-03 07:55:51 -0400

    switch to postgres

--- morphgnt_api/settings.py
+++ morphgnt_api/settings.py
@@ -77,12 +77,11 @@ WSGI_APPLICATION = 'morphgnt_api.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "morphgnt_api",
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
