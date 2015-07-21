ebfad1d0d51b322c2b35046e0d84b6fe96b51419
Brian Rosner <brosner@gmail.com>
2015-07-21 17:24:16 -0600

    added support for stdin in load_morphgnt

--- morphgnt_api/management/commands/load_morphgnt.py
+++ morphgnt_api/management/commands/load_morphgnt.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.core.management.base import BaseCommand
 
 from morphgnt_api.models import Word, Book, Paragraph, Sentence, Verse
@@ -59,89 +61,95 @@ class Command(BaseCommand):
                 sblgnt_id=v[1],
             ).save()
 
+        if filename == "-":
+            self.handle_stream(sys.stdin)
+        else:
+            with open(filename, "rb") as f:
+                self.handle_stream(f)
+
+    def handle_stream(self, f):
         prev_verse = None
         prev_sentence = None
         prev_paragraph = None
 
-        with open(filename, "rb") as f:
-            for line in f:
-                word_id, verse_id, paragraph_id, sentence_id, \
-                    pos, parse, \
-                    crit_text, text, word, norm, lemma, \
-                    dep_type, head = line.strip().split()
+        for line in f:
+            word_id, verse_id, paragraph_id, sentence_id, \
+                pos, parse, \
+                crit_text, text, word, norm, lemma, \
+                dep_type, head = line.strip().split()
 
-                Word(
-                    word_id=word_id,
+            Word(
+                word_id=word_id,
+                verse_id=verse_id,
+                paragraph_id=paragraph_id,
+                sentence_id=sentence_id,
+                pos=pos,
+                parse=parse,
+                crit_text=u(crit_text),
+                text=u(text),
+                word=u(word),
+                norm=u(norm),
+                lemma=u(lemma),
+                dep_type=dep_type,
+                head=head if head != "None" else None,
+            ).save()
+
+            if prev_verse is None:
+                prev_verse = Verse(
                     verse_id=verse_id,
-                    paragraph_id=paragraph_id,
+                    book_osis_id=books_by_sblgnt_id[int(verse_id[:2])],
+                    prev_verse=None,
+                    next_verse=None,
+                )
+                prev_verse.save()
+            elif prev_verse.verse_id != verse_id:
+                if prev_verse.verse_id[:2] == verse_id[:2]:
+                    prev_verse.next_verse = verse_id
+                    prev_verse.save()
+                prev_verse = Verse(
+                    verse_id=verse_id,
+                    book_osis_id=books_by_sblgnt_id[int(verse_id[:2])],
+                    prev_verse=prev_verse.verse_id,
+                    next_verse=None,
+                )
+                prev_verse.save()
+
+            if prev_sentence is None:
+                prev_sentence = Sentence(
                     sentence_id=sentence_id,
-                    pos=pos,
-                    parse=parse,
-                    crit_text=u(crit_text),
-                    text=u(text),
-                    word=u(word),
-                    norm=u(norm),
-                    lemma=u(lemma),
-                    dep_type=dep_type,
-                    head=head if head != "None" else None,
-                ).save()
-
-                if prev_verse is None:
-                    prev_verse = Verse(
-                        verse_id=verse_id,
-                        book_osis_id=books_by_sblgnt_id[int(verse_id[:2])],
-                        prev_verse=None,
-                        next_verse=None,
-                    )
-                    prev_verse.save()
-                elif prev_verse.verse_id != verse_id:
-                    if prev_verse.verse_id[:2] == verse_id[:2]:
-                        prev_verse.next_verse = verse_id
-                        prev_verse.save()
-                    prev_verse = Verse(
-                        verse_id=verse_id,
-                        book_osis_id=books_by_sblgnt_id[int(verse_id[:2])],
-                        prev_verse=prev_verse.verse_id,
-                        next_verse=None,
-                    )
-                    prev_verse.save()
-
-                if prev_sentence is None:
-                    prev_sentence = Sentence(
-                        sentence_id=sentence_id,
-                        book_osis_id=books_by_sblgnt_id[int(sentence_id[:2])],
-                        prev_sentence=None,
-                        next_sentence=None,
-                    )
+                    book_osis_id=books_by_sblgnt_id[int(sentence_id[:2])],
+                    prev_sentence=None,
+                    next_sentence=None,
+                )
+                prev_sentence.save()
+            elif prev_sentence.sentence_id != sentence_id:
+                if prev_sentence.sentence_id[:2] == sentence_id[:2]:
+                    prev_sentence.next_sentence = sentence_id
                     prev_sentence.save()
-                elif prev_sentence.sentence_id != sentence_id:
-                    if prev_sentence.sentence_id[:2] == sentence_id[:2]:
-                        prev_sentence.next_sentence = sentence_id
-                        prev_sentence.save()
-                    prev_sentence = Sentence(
-                        sentence_id=sentence_id,
-                        book_osis_id=books_by_sblgnt_id[int(sentence_id[:2])],
-                        prev_sentence=prev_sentence.sentence_id,
-                        next_sentence=None,
-                    )
-                    prev_sentence.save()
+                prev_sentence = Sentence(
+                    sentence_id=sentence_id,
+                    book_osis_id=books_by_sblgnt_id[int(sentence_id[:2])],
+                    prev_sentence=prev_sentence.sentence_id,
+                    next_sentence=None,
+                )
+                prev_sentence.save()
 
-                if prev_paragraph is None:
-                    prev_paragraph = Paragraph(
-                        paragraph_id=paragraph_id,
-                        book_osis_id=books_by_sblgnt_id[int(paragraph_id[:2])],
-                        prev_paragraph=None,
-                        next_paragraph=None,
-                    )
+            if prev_paragraph is None:
+                prev_paragraph = Paragraph(
+                    paragraph_id=paragraph_id,
+                    book_osis_id=books_by_sblgnt_id[int(paragraph_id[:2])],
+                    prev_paragraph=None,
+                    next_paragraph=None,
+                )
+                prev_paragraph.save()
+            elif prev_paragraph.paragraph_id != paragraph_id:
+                if prev_paragraph.paragraph_id[:2] == paragraph_id[:2]:
+                    prev_paragraph.next_paragraph = paragraph_id
                     prev_paragraph.save()
-                elif prev_paragraph.paragraph_id != paragraph_id:
-                    if prev_paragraph.paragraph_id[:2] == paragraph_id[:2]:
-                        prev_paragraph.next_paragraph = paragraph_id
-                        prev_paragraph.save()
-                    prev_paragraph = Paragraph(
-                        paragraph_id=paragraph_id,
-                        book_osis_id=books_by_sblgnt_id[int(paragraph_id[:2])],
-                        prev_paragraph=prev_paragraph.paragraph_id,
-                        next_paragraph=None,
-                    )
-                    prev_paragraph.save()
+                prev_paragraph = Paragraph(
+                    paragraph_id=paragraph_id,
+                    book_osis_id=books_by_sblgnt_id[int(paragraph_id[:2])],
+                    prev_paragraph=prev_paragraph.paragraph_id,
+                    next_paragraph=None,
+                )
+                prev_paragraph.save()
