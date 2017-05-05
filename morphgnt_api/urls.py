d00f8c0c7bdbe403c868710cac9fd064f0ffb4a5
Brian Rosner <brian@brosner.com>
2017-05-05 10:00:44 -0600

    upgraded deps and fixed 1.11 compat

--- morphgnt_api/urls.py
+++ morphgnt_api/urls.py
@@ -1,19 +1,22 @@
 from django.conf.urls import include, url
 
+from . import views
+
+
 urlpatterns = [
-    url(r"^v0/word/(?P<word_id>\d{11}).json$", "morphgnt_api.views.word", name="word"),
+    url(r"^v0/word/(?P<word_id>\d{11}).json$", views.word, name="word"),
 
-    url(r"^v0/paragraph/(?P<paragraph_id>\d{5}).json$", "morphgnt_api.views.paragraph", name="paragraph"),
-    url(r"^v0/sentence/(?P<sentence_id>\d{6}).json$", "morphgnt_api.views.sentence", name="sentence"),
-    url(r"^v0/verse/(?P<verse_id>\d{6}).json$", "morphgnt_api.views.verse", name="verse"),
+    url(r"^v0/paragraph/(?P<paragraph_id>\d{5}).json$", views.paragraph, name="paragraph"),
+    url(r"^v0/sentence/(?P<sentence_id>\d{6}).json$", views.sentence, name="sentence"),
+    url(r"^v0/verse/(?P<verse_id>\d{6}).json$", views.verse, name="verse"),
 
-    url(r"^v0/book/(?P<book_osis_id>\w+).json$", "morphgnt_api.views.book", name="book"),
+    url(r"^v0/book/(?P<book_osis_id>\w+).json$", views.book, name="book"),
 
-    url(r"^v0/root.json$", "morphgnt_api.views.root", name="root"),
+    url(r"^v0/root.json$", views.root, name="root"),
 
-    url(r"^loaderio-97c892ba1d7ce63eb626ca62a7a0a961/$", "morphgnt_api.views.loader_verification"),
+    url(r"^loaderio-97c892ba1d7ce63eb626ca62a7a0a961/$", views.loader_verification),
 
     url(r"^\.well-known/", include("letsencrypt.urls")),
 
-    url(r"^$", "morphgnt_api.views.home", name="home"),
+    url(r"^$", views.home, name="home"),
 ]
